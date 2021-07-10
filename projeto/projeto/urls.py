dd2f0e0ede611e0870dc67f8ed3fc14ab37e4d1e
Lucas <lucasnego0142@gmail.com>
2021-07-10 19:54:00 -0300

    teste loko

--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from server_remoto import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.menu_inicial, name = 'menu'),]
--- projeto/server_remoto/views.py
+++ projeto/server_remoto/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def menu_inicial(request):
+    return render(request, "menu_inicial.html", {})
