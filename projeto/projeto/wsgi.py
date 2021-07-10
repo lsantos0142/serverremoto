81d5b3fdaa4b16dc56ba16fe5c0b2d52298e88f1
Lucas <lucasnego0142@gmail.com>
2021-07-10 20:22:15 -0300

    por favor

--- projeto/projeto/wsgi.py
+++ projeto/projeto/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projeto.settings")
 
 from django.core.wsgi import get_wsgi_application
 
