dfffbb217cd805966431918d7d0794f3937aa52d
Lucas <lucasnego0142@gmail.com>
2021-07-13 14:18:07 -0300

    teste sincronizar

--- projeto/server_remoto/views.py
+++ projeto/server_remoto/views.py
@@ -1,7 +1,9 @@
+import json
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Paciente
+from django.core import serializers
 
 # Create your views here.
 
@@ -10,5 +12,11 @@ def menu_inicial(request):
 
 def sincronizar(request):
     if request.method == "POST":
-        x = request.POST
-    return JsonResponse({x})
+        x = {'foo': 'bar'}
+        req_paciente = Paciente(paciente=x)
+        req_paciente.save()
+    numero_de_pacientes = Paciente.objects.all().count()
+    for i in range(0,numero_de_pacientes):
+        paci = Paciente.objects.all()[i]
+        serialized_obj = serializers.serialize('json', [paci])
+    return JsonResponse({Paciente.objects.all()})
