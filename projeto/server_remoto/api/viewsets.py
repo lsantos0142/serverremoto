2093c4f2e585c1d449ff6913500f4b15e1bdb8dd
Lucas <lucasnego0142@gmail.com>
2021-07-16 00:29:01 -0300

    patch paciente

--- projeto/server_remoto/api/viewsets.py
+++ projeto/server_remoto/api/viewsets.py
@@ -1,10 +1,21 @@
 from rest_framework import viewsets
 from server_remoto import models
 from server_remoto.api import serializers
+from rest_framework.response import Response
 
 class PacienteViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.PacienteSerializer
     queryset = models.Paciente.objects.all()
+    def patch(self, request, *args, **kwargs):
+        if models.Paciente.objects.filter(CPF__iexact=request.data.get('CPF')):
+            paciente = models.Paciente.objects.filter(CPF__iexact=request.data.get('CPF'))[0]
+        elif models.Paciente.objects.filter(CNS__iexact=request.data.get('CNS')):
+            paciente = models.Paciente.objects.filter(CPF__iexact=request.data.get('CNS'))[0]
+        serializer = serializers.PacienteSerializer(instance=paciente, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
 
 class ImunizacaoViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ImunizacaoSerializer
